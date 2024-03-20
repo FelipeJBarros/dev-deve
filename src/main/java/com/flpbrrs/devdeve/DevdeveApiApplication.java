c0fa209d369227c3bed5218ca80f29b89bf00322
Felipe Barros <felipebarros.engh@gmail.com>
2024-03-20 08:54:02 -0300

    feat: initial files

---
+++ src/main/java/com/flpbrrs/devdeve/DevdeveApiApplication.java
@@ -0,0 +1,13 @@
+package com.flpbrrs.devdeve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdeveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdeveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flpbrrs/devdeve/DevdeveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flpbrrs.devdeve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdeveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
