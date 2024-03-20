c5fe44cc5cc0f6672d20bcef3ded132e36992034
Felipe Barros <felipebarros.engh@gmail.com>
2024-03-20 09:39:49 -0300

    feat: creating find by id route for client

--- src/main/java/com/flpbrrs/devdeve/api/controller/ClientController.java
+++ src/main/java/com/flpbrrs/devdeve/api/controller/ClientController.java
@@ -3,20 +3,28 @@ package com.flpbrrs.devdeve.api.controller;
 import com.flpbrrs.devdeve.domain.models.Client;
 import com.flpbrrs.devdeve.domain.repositories.ClientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("clients")
 @AllArgsConstructor
 public class ClientController {
     private final ClientRepository clientRepository;
-
     @GetMapping
     public List<Client> list() {
         return clientRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getById(@PathVariable UUID id) {
+        Optional<Client> client = clientRepository.findById(id);
+        return client.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
