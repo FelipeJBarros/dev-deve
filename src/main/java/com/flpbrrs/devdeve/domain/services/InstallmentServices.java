987db73e8ec5f8b59df77155f0c36a2c6a027854
Felipe Barros <felipebarros.engh@gmail.com>
2024-03-21 10:15:57 -0300

    fear: creating installment services

---
+++ src/main/java/com/flpbrrs/devdeve/domain/services/InstallmentServices.java
@@ -0,0 +1,29 @@
+package com.flpbrrs.devdeve.domain.services;
+
+import com.flpbrrs.devdeve.domain.exceptions.DomainException;
+import com.flpbrrs.devdeve.domain.models.Client;
+import com.flpbrrs.devdeve.domain.models.Installment;
+import com.flpbrrs.devdeve.domain.repositories.InstallmentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class InstallmentServices {
+    private final InstallmentRepository installmentRepository;
+    private final ClientServices clientServices;
+
+    @Transactional
+    public Installment save(Installment installment) {
+        if (installment.getId() != null) {
+            throw new DomainException("installments to be created must not have an id");
+        }
+
+        Client client = clientServices.find(installment.getClient().getId());
+
+        installment.setClient(client);
+
+        return installmentRepository.save(installment);
+    }
+}
