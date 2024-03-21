63960f132ebe5ba8d9cbf7a08e1cf0ea90da4950
Felipe Barros <felipebarros.engh@gmail.com>
2024-03-21 08:50:54 -0300

    feat: create installment entity

---
+++ src/main/java/com/flpbrrs/devdeve/domain/models/Installment.java
@@ -0,0 +1,38 @@
+package com.flpbrrs.devdeve.domain.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "installments")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Installment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private Client client;
+
+    @Column(name = "total_amount", nullable = false)
+    private BigDecimal totalAmount;
+
+    @Column(name = "job_description", nullable = false)
+    private String jobDescription;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
